66596cc62239cd91d13586b9109646f9136f38d0
haggagdev <haggagdev@gmail.com>
2023-08-04 22:39:23 +0300

    create main menu and create page

---
+++ create_db.sh
@@ -0,0 +1,37 @@
+
+# Create database
+
+
+if [ $# -ne 1 ]
+then
+    echo "Invalid number of arguments"
+    exit
+fi
+
+if [ -d $1 ]
+then
+    echo "Database already exists"
+    exit
+fi
+
+# check if first character is a letter
+
+if [[ $1 =~ ^[a-zA-Z] ]]
+then
+    :
+else
+    echo "Invalid database name"
+    exit
+fi
+
+# check if the database full name is valid
+
+if [[ $1 =~ [^a-zA-Z0-9] ]]
+then
+    echo "Invalid database name"
+    exit
+fi
+
+
+
+mkdir $1
--- main.sh
+++ main.sh
@@ -1,2 +1,35 @@
 #! /usr/bin/bash
 
+## this is the main script that will be used to run the project
+PS3=">>> "
+
+
+select choice in "Create Database" "Use Database" "Show Databases" "Drop Database" "Exit"
+do
+    case $choice in
+        "Create Database")
+            echo "Enter the name of the database: "
+            read -r dbname
+            ./create_db.sh $dbname
+            ;;
+        "Use Database")
+            echo "Enter the name of the database: "
+            read dbname
+            ./use_db.sh $dbname
+            ;;
+        "Show Databases")
+            ./show_db.sh
+            ;;
+        "Drop Database")
+            echo "Enter the name of the database: "
+            read dbname
+            ./drop_db.sh $dbname
+            ;;
+        "Exit")
+            exit
+            ;;
+        *)
+            echo "Invalid choice"
+            ;;
+    esac
+done
