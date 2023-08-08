cafe753b61bb0f5fe3d6c2b08b26cb15b176e139
haggagdev <haggagdev@gmail.com>
2023-08-08 03:28:50 +0300

    modify create table

--- create_tb.sh
+++ create_tb.sh
@@ -1,11 +1,75 @@
 #! /usr/bin/bash 
 
-# create table script
 
+# Functions 
+
+function set_primary_key {
+
+    select pk in $(cat $tbname".hgtb.config" | cut -d ":" -f 1); do
+
+        if [ $pk = "exit" ]
+        then
+            exit
+        fi
+
+        if [[ ! $pk =~ ^[a-zA-Z][a-zA-Z0-9_]*$ ]]
+        then
+            echo -e $red"Syntax is incorrect, column name must start with a letter and can contain only letters, numbers and underscores"$reset
+            continue 1
+        fi
+
+        if [[ ! $(cat $tbname".hgtb.config" | cut -d ":" -f 1) ]]
+        then
+            echo -e $red"Column name is incorrect, enter exit to back to main menu "$reset
+            continue 1
+        fi
+
+        break
+       
+    done
+
+   
+
+
+    read -r -p "Enter the name of the column you want to be the primary key, enter exit to back to main menu" pk
+    if [ $1 = "exit" ]
+    then
+        exit
+    fi
+
+    if [[ ! $1 =~ ^[a-zA-Z][a-zA-Z0-9_]*$ ]]
+    then
+        echo -e $red"Syntax is incorrect, column name must start with a letter and can contain only letters, numbers and underscores"$reset
+        continue 1
+    fi
+
+    if [[ ! $(cat $tbname".hgtb.config" | grep -w $pk) ]]
+    then
+        echo -e $red"Column name is incorrect, enter exit to back to main menu "$reset
+        continue 1
+    fi
+
+    echo $pk >> $tbname".hgtb.config"
+
+    
+
+    
+}
+
+# end of functions
+
+
+
+
+# color variables
 red='\033[31m'
 green='\033[32m'
 reset='\033[0m'
 
+# end of color variables
+
+
+# create table script
 cd $1
 
 while true 
@@ -14,12 +78,12 @@ do
 read -r -p "Enter the name of the table: " tbname
 
 # check if syntax is correct
-# explain the syntax
 
-if [ $tbname = "exit" ]
+if [ $tbname = "exit" -o $tbname = "Exit" ]
 then
     exit
 fi
+
 
 if [[ ! $tbname =~ ^[a-zA-Z][a-zA-Z0-9_]*$ ]]
 then
@@ -38,75 +102,34 @@ else
     read -r -p "Enter the number of columns: " colnum
     for (( i = 0; i < colnum; i++ )); do
         read -r -p "Enter the name of column $((i+1)): " colname
-        read -r -p "Enter the datatype of column $((i+1)): " coltype
+        read -r -p "Enter the datatype of column  $((i+1)): " coltype
+        select coltype in "string" "integer"; do
+            case $coltype in
+                string ) coltype="string"; break;;
+                integer ) coltype="integer"; break;;
+                * ) echo "Please choose 1 or 2";;
+            esac
+        done
         echo -n "$colname:$coltype:" >> $tbname".hgtb.config"
     done
     echo $green"Table created successfully"$reset
 fi
 
+
+
 read -r -p "Do you want to set a primary key? (y/n) " ans
 
-if [ $ans = "y" ]
-  set_primary_key ( $tbname )
-then
-    break
+if [ $ans = "y" ];then
+  set_primary_key  $tbname 
 fi
 
-echo -e "Enter the name of the column you want to be the primary key, enter exit to back to main menu"
-read -r pk
-if [ $pk = "exit" ]
-then
-    exit
+read -r -p "do you want to create another table? (y/n) \c" cont
+if [ $ans = "n" ];then
+  break
 fi
-
-if [[ ! $pk =~ ^[a-zA-Z][a-zA-Z0-9_]*$ ]]
-then
-    echo -e $red"Syntax is incorrect, column name must start with a letter and can contain only letters, numbers and underscores, enter exit to back to main menu "$reset
-    continue 1
-fi
-
-if [[ ! $(cat $tbname".hgtb.config" | grep -w $pk) ]]
-then
-    echo -e $red"Column name is incorrect, enter exit to back to main menu "$reset
-    continue 1
-fi
-
-echo $pk >> $tbname".hgtb.config"
-
 
 
 done
 
-
-function set_primary_key ( $tbname ) {
-
-    while true
-    do
-
-
-    read -r -p "Enter the name of the column you want to be the primary key, enter exit to back to main menu" pk
-    if [ $pk = "exit" ]
-    then
-        exit
-    fi
-
-    if [[ ! $pk =~ ^[a-zA-Z][a-zA-Z0-9_]*$ ]]
-    then
-        echo -e $red"Syntax is incorrect, column name must start with a letter and can contain only letters, numbers and underscores"$reset
-        continue 1
-    fi
-
-    if [[ ! $(cat $tbname".hgtb.config" | grep -w $pk) ]]
-    then
-        echo -e $red"Column name is incorrect, enter exit to back to main menu "$reset
-        continue 1
-    fi
-
-    echo $pk >> $tbname".hgtb.config"
-
-    break
-
-    done
-}
 
 
